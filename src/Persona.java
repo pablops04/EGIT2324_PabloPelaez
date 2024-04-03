7686614ca680a5a3f4aa085935ad1437952c50ae
Pablo Peláez <a_pablo.pelaez.sanchez@iespablopicasso.es>
2024-04-03 11:01:07 +0200

    Creación de la clase persona y registro de la misma

---
+++ src/Persona.java
@@ -0,0 +1,44 @@
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    // Constructor de la clase Persona
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    // Método Cumpleaños que aumenta la edad en 1 y muestra por pantalla el valor actualizado
+    public void Cumpleaños() {
+        edad++; 
+        System.out.println("¡Felicidades! Ahora tienes " + edad + " años.");
+    }
+
+
+    // Métodos getters y setters 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
